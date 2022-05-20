68c59a5a82ff47568258b5798cf7b97ba90aa75f
Herivin Simmasalam <hsimma0@gmail.com>
2022-05-20 17:05:12 -0400

    I lied I forgot something else, last commit

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 //EXPRESS ROUTE
 const express = require('express');
 const app = express();
+const drinks = require('./models/drinks')
 const port= 3011;
 
-//START OF THE PORT (localhost:3010/)
+//START OF THE PORT TEST (localhost:3011/)
 app.get('/', (req, res) => {
     res.send('Hello, its GitPub')
 });
 
 //INDEX DRINK (Setting up your index view to show your drinks data)
-app.get('drink', (req, res) => {
+app.get('/drink', (req, res) => {
     res.render('drink_index.ejs', {totallDrinks : drinks})
 });
 
@@ -19,9 +20,9 @@ app.get('drink', (req, res) => {
 // });
 
 // SHOW DRINK (Setting up show route)
-app.get('/drinks/:id', (req,res) => {
-    res.render(req.params.id);
-});
+// app.get('/drinks/:id', (req,res) => {
+//     res.render(req.params.id);
+// });
 
 // SHOW FOOD (Setting up show route)
 // app.get('/food/:id', (req,res) => {
@@ -29,9 +30,9 @@ app.get('/drinks/:id', (req,res) => {
 // });
 
 // RENDER DRINK ()
-app.get('/drinks/:id', (req, res) => {
-    res.render('drinks_show.ejs', {drinks: drinks[req.params.id]});
-});
+// app.get('/drinks/:id', (req, res) => {
+//     res.render('drinks_show.ejs', {drinks: drinks[req.params.id]});
+// });
 
 // RENDER FOOD ()
 // app.get('/food/:id', (req, res) => {
@@ -39,9 +40,9 @@ app.get('/drinks/:id', (req, res) => {
 // });
 
 //LINKING DRINK SHOW AND INDEX ()
-app.get('/drinks/:id', (req, res) => {
-    res.render('drinks_show.ejs', {drink: drinks[req.params.id]});
-});
+// app.get('/drinks/:id', (req, res) => {
+//     res.render('drinks_show.ejs', {drink: drinks[req.params.id]});
+// });
 
 //LINKING FOOD SHOW AND INDEX ()
 // app.get('/food/:id', (req, res) => {
